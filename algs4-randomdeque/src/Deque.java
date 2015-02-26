d784cc2aedf9af1160ee5d78fe41957184e04b16
Nuno Esteves <nesteves@users.noreply.github.com>
2015-02-26 21:17:35 +0000

    Implement the Deque class Node(inner class), iterator, and addFirst().

--- algs4-randomdeque/src/Deque.java
+++ algs4-randomdeque/src/Deque.java
@@ -11,12 +11,17 @@ import java.util.Iterator;
  */
 public class Deque<Item> implements Iterable<Item> {
   
+  Node<Item> first;
+  Node<Item> last;
+  int size = 0;
+  
   public Deque() {
-    
+    first = null;
+    last = null;
   }
   
   public boolean isEmpty() {
-    return false;
+    return first == null;
   }
   
   public int size() {
@@ -24,11 +29,19 @@ public class Deque<Item> implements Iterable<Item> {
   }
   
   public void addFirst(Item item) {
+    if (item == null) throw new java.util.NoSuchElementException();
     
+    Node<Item> newNode = new Node<Item>();
+    newNode.value = item;
+    newNode.next = first;
+    newNode.previous = null;
+    first = newNode;
+    
+    size++;
   }
   
   public void addLast(Item item) {
-    
+    if (item == null) throw new java.util.NoSuchElementException();
   }
   
   public Item removeFirst() {
@@ -40,30 +53,58 @@ public class Deque<Item> implements Iterable<Item> {
   }
   
   public Iterator<Item> iterator() {
-    return new DequeIterator<Item>();
+    return new DequeIterator<Item>(first);
   }
   
+  /**
+   * Class used to reference elements in the Deque
+   *
+   * @param <Item> Generic type
+   */
+  private static class Node<Item> {
+    private Item value;
+    private Node<Item> next;
+    private Node<Item> previous;
+  }
   
   /**
    * Iterator implementation for the class Deque
    *
    * @param <E> Generic type
    */
-  public class DequeIterator<E> implements Iterator<E> {
+  public class DequeIterator<Item> implements Iterator<Item> {
     
+    private Node<Item> current;
     
-    
-    public boolean hasNext() {
-      return false;
+    public DequeIterator(Node<Item> first) {
+      current = first;
     }
     
-    public E next() {
+    public boolean hasNext() {
+      return current != null;
+    }
+    
+    public Item next() {
       if (!hasNext()) throw new java.util.NoSuchElementException();
-      return null;
+      
+      Item value = current.value;
+      current = current.next;
+      return value;
     }
     
     public void remove() { throw new java.lang.UnsupportedOperationException(); }
   }
   
-  
+  public static void main(String[] args) {
+    Deque<Integer> testDq = new Deque<Integer>();
+    
+    testDq.addFirst(1);
+    testDq.addFirst(2);
+    System.out.println(testDq.size());
+    
+    System.out.println("\nPrint all the Deque elements:\n");
+    for (int i : testDq) {
+      System.out.print(" " + i);
+    }
+  }
 }
