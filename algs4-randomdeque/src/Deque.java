23a20ccea023f965ee28a7f1af0eb1226a8e625e
Nuno Esteves <nesteves@users.noreply.github.com>
2015-02-28 23:46:56 +0000

    Class Deque - correct the implementation of remove() for the case of removing the last element

--- algs4-randomdeque/src/Deque.java
+++ algs4-randomdeque/src/Deque.java
@@ -68,7 +68,7 @@ public class Deque<Item> implements Iterable<Item> {
     Item returnVal = first.value;
     
     first = first.next;
-    first.previous = null;
+    if(first != null) first.previous = null;
     size--;
     
     return returnVal;
@@ -83,7 +83,7 @@ public class Deque<Item> implements Iterable<Item> {
     Item returnVal = last.value;
     
     last = last.previous;
-    last.next = null;
+    if (last != null) last.next = null;
     size--;
     
     return returnVal;
