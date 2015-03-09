0588d2de5d5c6cb0a59eaeade35cc94163737ce8
Nuno Esteves <nesteves@users.noreply.github.com>
2015-03-09 21:44:41 +0000

    Class Fast - code to set up the points[] array.

--- algs4-collinearpoints/src/Fast.java
+++ algs4-collinearpoints/src/Fast.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 /**
  * Class used to find collinear points faster than the Brute
  * class.
@@ -46,6 +48,18 @@ public class Fast {
   }
 
   public static void main(String[] args) {
+if (args.length != 1) throw new java.lang.IllegalArgumentException();
+    
+    LinkedList<Point[]> segments = new LinkedList<Point[]>();
+       
+    // Read in all the Points from the input file into an array
+    In input = new In(args[0]);
+    Point[] points = new Point[input.readInt()];
+    
+    for (int i = 0; i < points.length; i++) {
+      points[i] = new Point(input.readInt(), input.readInt());
+    }
+    
     
   }
 
