a8ce130c7fb09578e598769c8d37c583eb3e36cb
Nuno Esteves <nesteves@users.noreply.github.com>
2015-03-09 22:34:28 +0000

    Class Fast - implement the main method: iterate through each point, order by slope, select the longest segment; print the results.

--- algs4-collinearpoints/src/Fast.java
+++ algs4-collinearpoints/src/Fast.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
@@ -48,18 +49,65 @@ public class Fast {
   }
 
   public static void main(String[] args) {
-if (args.length != 1) throw new java.lang.IllegalArgumentException();
+    if (args.length != 1) throw new java.lang.IllegalArgumentException();
     
     LinkedList<Point[]> segments = new LinkedList<Point[]>();
        
     // Read in all the Points from the input file into an array
     In input = new In(args[0]);
     Point[] points = new Point[input.readInt()];
+    Point[] sortedPoints = new Point[points.length];
     
     for (int i = 0; i < points.length; i++) {
       points[i] = new Point(input.readInt(), input.readInt());
+      sortedPoints[i] = points[i]; // Point is immutable, and we only need the reference
     }
     
+    for (Point p : points) {
+      Arrays.sort(sortedPoints, p.SLOPE_ORDER);
+      
+      // find the longest segment in the sortedPoints array
+      int s = 0; // start point
+      int l = 0; // length
+      int sMax = 0;
+      int lMax = 0;
+      for (int i = 0; i < sortedPoints.length; i++) {
+        
+        if (i == s || p.slopeTo(sortedPoints[s]) == p.slopeTo(sortedPoints[i])) {
+          l++;
+        }
+        else {
+          if (l >= lMax) {
+            sMax = s;
+            lMax = l;
+          }
+          s = i;
+          l = 0;
+        }
+      }
+      // Put the points into an array
+      if (lMax >= 2) {
+        //System.out.println("Start point: " + sMax + ". Length of semgent: " + lMax + ".");
+        Point[] segment = new Point[lMax + 1];
+        for (int i = sMax; i < sMax + lMax; i++) {
+          segment[i - sMax] = sortedPoints[i];
+        }
+        segment[lMax] = p;
+        Arrays.sort(segment);
+        segments.add(segment);
+      }
+    }
+    
+    // Set up the grid and draw the points
+    StdDraw.setXscale(0, 32768);
+    StdDraw.setYscale(0, 32768);
+    drawPoints(points);
+    
+    // Print out the collinear segments and draw them
+    for (Point[] s : segments) {
+      System.out.println(stringSegment(s));
+      drawSegments(s);
+    }
     
   }
 
