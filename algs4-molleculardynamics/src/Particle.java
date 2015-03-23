08cf0d40d67c76e681c53e13b87d641d5fee7f17
Nuno Esteves <nesteves@users.noreply.github.com>
2015-03-23 20:46:55 +0000

    Class Particle - implement basic API for the class (method stubs).

---
+++ algs4-molleculardynamics/src/Particle.java
@@ -0,0 +1,90 @@
+
+/**
+ * Class that represents a 2-dimensional
+ * disc with physical properties such as
+ * mass and velocity
+ */
+public class Particle {
+  private double rx, ry;
+  private double vx, vy;
+  private final double radius;
+  private final double mass;
+  private int count;
+  
+  /**
+   * Basic parameterized constructor
+   * @param rx position in the x-axis
+   * @param ry position in the y-axis
+   * @param vx velocity in the x-axis
+   * @param vy velocity in the y-axis
+   * @param radius radius of the particle
+   * @param mass mass of the particle
+   */
+  public Particle(double rx, double ry, double vx, double vy, double radius,
+      double mass) {
+    super();
+    this.rx = rx;
+    this.ry = ry;
+    this.vx = vx;
+    this.vy = vy;
+    this.radius = radius;
+    this.mass = mass;
+  }
+  
+  /**
+   * Moves the instance of Particle an amount
+   * depending on its physical properties and the given
+   * time lapse
+   * @param dt amount of time lapsed
+   */
+  public void move(double dt) {
+    
+  }
+  
+  /**
+   * Draws the particle in the board
+   */
+  public void draw() {
+    
+  }
+  
+  /**
+   * Predicts a collision with another particle
+   * @param that the particle against which to test for an eventual
+   * collision
+   * @return the time it will take for both particles to collide
+   */
+  public double timeToHit(Particle that) {
+    return 0.0;
+  }
+  
+  /**
+   * Predicts a collision with a vertical wall
+   * @return the time it will take for this Particle to
+   * collide with a vertical wall
+   */
+  public double timeToHitVerticalWall() {
+    return 0.0;
+  }
+  
+  /**
+   * Predicts a collision with a horizontal wall
+   * @return the time it will take for this Particle to
+   * collide with a vertical wall
+   */
+  public double timeToHitHorizontallWall() {
+    return 0.0;
+  }
+  
+  public void bounceOff(Particle that) {
+    
+  }
+  
+  public void bounceOffVerticallWall() {
+    
+  }
+  
+  public void bounceOffHorizontalWall() {
+    
+  }
+}
