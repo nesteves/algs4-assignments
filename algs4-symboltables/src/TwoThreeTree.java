e66f481e46fb09a74759d7b35079c9c8b997a07d
Nuno Esteves <nesteves@users.noreply.github.com>
2015-04-29 23:39:36 +0100

    Class TwoThreeTree - add comments to each method

--- algs4-symboltables/src/TwoThreeTree.java
+++ algs4-symboltables/src/TwoThreeTree.java
@@ -18,22 +18,54 @@ public class TwoThreeTree<Key extends Comparable<Key>, Value> {
     boolean color; 
   }
   
+  /**
+   * Used to get the value corresponding to a particular key
+   * @param key
+   * @return the corresponding value
+   */
   public Value get(Key key) {
     throw new java.lang.UnsupportedOperationException("Not implemented.");
   }
   
+  /**
+   * Makes the tree "lean to the left" for
+   * the given node. It affects a 3-node
+   * @param n he node that has a red link to its right,
+   * said to be "leaning right"
+   * @return the node that now has a red link to its left,
+   * "leaning left"
+   */
   private Node rotateLeft(Node n) {
     throw new java.lang.UnsupportedOperationException("Not implemented.");
   }
   
+  /**
+   * This method mirrors rotateLeft
+   * @param n the node that has a red link to its left
+   * @return the node that now has a red link to its right
+   */
   private Node rotateRight(Node n) {
     throw new java.lang.UnsupportedOperationException("Not implemented.");
   }
   
+  /**
+   * Switches the colors of the links connected to the given node.
+   * Links connecting to the child nodes are switched to RED, while
+   * the link connecting to the parent node is switched to BLACK
+   * @param n the node for which to switch the link colors
+   */
   private void flipColors(Node n) {
     throw new java.lang.UnsupportedOperationException("Not implemented.");
   }
   
+  /**
+   * Test the color of the link to the parent
+   * Node
+   * @param n the node whose link to the parent
+   * node is to be tested
+   * @return true if the link to the parent Node is RED,
+   * false otherwise
+   */
   private boolean isRed(Node n) {
     if (n == null) return false;
     return n.color == RED;
