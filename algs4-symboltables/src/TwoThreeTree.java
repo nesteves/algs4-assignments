81f2a0916c09cec81d7dda38ecf11a46b9c39875
Nuno Esteves <nesteves@users.noreply.github.com>
2015-04-29 23:27:35 +0100

    Class TwoThreeTree - implement basic API

--- algs4-symboltables/src/TwoThreeTree.java
+++ algs4-symboltables/src/TwoThreeTree.java
@@ -1,4 +1,41 @@
 
+/**
+ * Implementation of a 2-3 Tree, using a left-leaning red-black tree
+ */
 public class TwoThreeTree<Key extends Comparable<Key>, Value> {
-
+  
+  private static final boolean RED = true;
+  private static final boolean BLACK = false;
+  
+  /**
+   * Represents a node in the left-leaning red-black tree
+   */
+  private class Node {
+    Key key;
+    Value value;
+    Node left;
+    Node right;
+    boolean color; 
+  }
+  
+  public Value get(Key key) {
+    throw new java.lang.UnsupportedOperationException("Not implemented.");
+  }
+  
+  private Node rotateLeft(Node n) {
+    throw new java.lang.UnsupportedOperationException("Not implemented.");
+  }
+  
+  private Node rotateRight(Node n) {
+    throw new java.lang.UnsupportedOperationException("Not implemented.");
+  }
+  
+  private void flipColors(Node n) {
+    throw new java.lang.UnsupportedOperationException("Not implemented.");
+  }
+  
+  private boolean isRed(Node n) {
+    if (n == null) return false;
+    return n.color == RED;
+  }
 }
